c26c33b551eb9ea8fe7514fd0f46b898ec3d085e
Marti <martiespinosa99@gmail.com>
2023-08-26 23:51:12 +0200

    variables y constantes

---
+++ PrimerosPasos/src/VariablesYConstantes.java
@@ -0,0 +1,28 @@
+public class VariablesYConstantes {
+    public static void main(String[] args) throws Exception {
+        
+        // VARIABLES
+        // Declaración e Iniciación de la variable edad1 en diefentes líneas:
+        byte edad1;
+        edad1 = 25;
+        System.out.println(edad1);
+
+        // Declaración e Iniciación de la variable edad2 en la misma línea:
+        byte edad2 = 30;
+        System.out.println(edad2);
+
+
+
+        // CONSTANTES
+        // Declaración e Iniciación de la constante edad3 en diefentes líneas:
+        final byte edad3;
+        edad3 = 50;
+        System.out.println(edad3);
+
+        // Declaración e Iniciación de la constante edad4 en la misma línea:
+        final byte edad4 = 55;
+        System.out.println(edad4);
+        
+    }
+}
+
