f600c9cf73d60be03fd250cc53cf89fb45f1ea7c
Marti <martiespinosa99@gmail.com>
2023-08-27 00:10:07 +0200

    operadores acabado ¿?

--- PrimerosPasos/src/Operadores.java
+++ PrimerosPasos/src/Operadores.java
@@ -1,7 +1,49 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
 
-        
+        // SUMA
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+        System.out.println(c);
+
+        // RESTA
+        int d = 15;
+        int e = 5;
+        int f = d - e;
+        System.out.println(f);
+
+        // MULTIPLICACIÓN
+        int g = 5;
+        int h = 5;
+        int i = g * h;
+        System.out.println(i);
+
+        // DIVISIÓN
+        int j = 90;
+        int k = 3;
+        int l = j / k;
+        System.out.println(l);
+
+        // INCREMENTO EN 1
+        int m = 2;
+        m ++;
+        System.out.println(m);
+
+        // DECREMENTO EN 1
+        int n = 2;
+        n --;
+        System.out.println(n);
+
+        // INCREMENTO EN n
+        int o = 2;
+        o += 6;
+        System.out.println(o);
+
+        // DECREMENTO EN n
+        int p = 10;
+        p -= 2;
+        System.out.println(p);
 
     }
 }
