344eafa54e8e23759b365292675b9f3a514917a9
Marti <martiespinosa99@gmail.com>
2023-08-26 23:18:59 +0200

    Los 2 proyectos en la misma carpeta PrimerosPasos

--- PrimerosPasos/src/Variables.java
+++ PrimerosPasos/src/Variables.java
@@ -1,4 +1,4 @@
-public class App {
+public class Variables {
     public static void main(String[] args) throws Exception {
         
         // Declaración e Iniciación de la variable edad1 en dierentes líneas:
@@ -12,3 +12,4 @@ public class App {
         
     }
 }
+
