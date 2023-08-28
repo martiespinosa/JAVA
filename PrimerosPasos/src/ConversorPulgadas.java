fa0bff3595c84b474de7eee6c356f09ea8028cfe
Marti <martiespinosa99@gmail.com>
2023-08-28 20:30:05 +0200

    convertidor pulgadas

---
+++ PrimerosPasos/src/ConversorPulgadas.java
@@ -0,0 +1,14 @@
+public class ConversorPulgadas {
+    public static void main(String[] args) throws Exception {
+
+        final double a_pulgadas = 2.54;
+
+        double cm, resultado; // Declaración de dos variables en la misma linea para iniciarlas más adelante
+
+        cm = 9;
+        resultado = cm / a_pulgadas;
+
+        System.out.println(resultado);
+
+    }
+}
