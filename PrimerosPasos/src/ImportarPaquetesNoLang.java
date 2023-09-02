f8f2d732c8565b57ff72d31956529c0f7dd72e7d
Marti <martiespinosa99@gmail.com>
2023-09-02 11:16:40 +0200

    importar paquetes que no pertenezcan a java.lang

---
+++ PrimerosPasos/src/ImportarPaquetesNoLang.java
@@ -0,0 +1,16 @@
+//import java.util.*; // importar todas las clases del paquete java.util
+
+// https://docs.oracle.com/javase/7/docs/api/ (java API)
+
+// todas las clases del paquete java.lang (Math, String, ...) no hace falta importarlas ya que están disponibles persé
+
+public class ImportarPaquetesNoLang {
+    public static void main(String[] args) throws Exception {
+
+        //Scanner mi_objeto1; // Scanner es una clase de java.util
+        //Locale mi_objeto2; // Locale es una clase de java.util
+        
+        
+        
+    }
+}
