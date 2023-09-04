24e17d67d4cdc68cb8c206b6cc9548cb04b3e7ef
Marti <martiespinosa99@gmail.com>
2023-09-04 19:37:19 +0200

    bucle do while

---
+++ PrimerosPasos/src/BucleDoWhile.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class BucleDoWhile {
+    public static void main(String[] args) {
+        
+        String genero = "";
+
+        do {
+            genero = JOptionPane.showInputDialog("Introduce tu género (H/M)");
+        } while (genero.equalsIgnoreCase("H") == false && genero.equalsIgnoreCase("M") == false);
+
+        int altura = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu altura en cm"));
+        int peso_ideal = 0;
+
+        if (genero.equalsIgnoreCase("H")) {
+            peso_ideal = altura - 110;
+        } else if (genero.equalsIgnoreCase("M")) {
+            peso_ideal = altura - 120;
+        }
+
+        System.out.println("Tu peso ideal orientativo es: " + peso_ideal + " kg");
+
+    }
+}
