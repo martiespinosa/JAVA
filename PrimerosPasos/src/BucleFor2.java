93f607cda72c513c837457edacd51f0d37c22e40
Marti <martiespinosa99@gmail.com>
2023-09-04 21:31:22 +0200

    comprueba si hay solo una arroba en el email

--- PrimerosPasos/src/BucleFor2.java
+++ PrimerosPasos/src/BucleFor2.java
@@ -5,17 +5,17 @@ import javax.swing.JOptionPane;
 public class BucleFor2 {
     public static void main(String[] args) {
         
-        boolean arroba = false;
+        int arroba = 0;
         
         String mail = JOptionPane.showInputDialog("Introduce tu correo electrónico");
 
         for (int i = 0; i < mail.length(); i++) {
             if (mail.charAt(i) == '@') {
-                arroba = true;
+                arroba ++;
             }
         }
 
-        if (arroba == true) {
+        if (arroba == 1) {
             System.out.println("Email correcto");
         } else {
             System.out.println("Email incorrecto");
