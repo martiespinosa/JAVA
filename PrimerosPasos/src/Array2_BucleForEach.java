ae756e5c78aa7a818cb37918314e575d77a9e258
Marti <martiespinosa99@gmail.com>
2023-09-05 16:02:00 +0200

    bucle for each con array de paises

---
+++ PrimerosPasos/src/Array2_BucleForEach.java
@@ -0,0 +1,13 @@
+// BUCLE FOR EACH Y ARRAYS
+
+public class Array2_BucleForEach {
+    public static void main(String[] args) {
+        
+        String [] paises = {"España", "México", "Colombia", "Perú", "Chile", "Argentina", "Ecuador", "Venezuela"};
+
+        for (String i: paises) {
+            System.out.println(i);
+        }
+
+    }
+}
