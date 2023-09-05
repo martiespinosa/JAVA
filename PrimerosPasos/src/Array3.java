ec683cbc7759379313dc049e06eb013220dc3d0b
Marti <martiespinosa99@gmail.com>
2023-09-05 16:13:21 +0200

    array desde joptionpane y bucle for each

---
+++ PrimerosPasos/src/Array3.java
@@ -0,0 +1,19 @@
+// ARRAY CON LOS VALORES ENTRADOS DESDE JOPTIONPANE
+
+import javax.swing.JOptionPane;
+
+public class Array3 {
+    public static void main(String[] args) {
+        
+        String [] paises = new String [8];
+
+        for (int i = 0; i < paises.length; i++) {
+            paises[i] = JOptionPane.showInputDialog("Introduce país " + (i+1));
+        }
+
+        for (String i: paises) {
+            System.out.println(i);
+        }
+
+    }
+}
